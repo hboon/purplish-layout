8a8f5868348bc764d60bb0e5b18df48057e24c56
Hwee-Boon Yar <hboon@motionobj.com>
2015-07-23 17:19:09 +0800

    #filled_by: accepts an inset

--- lib/purplish-layout/can_be_constrained.rb
+++ lib/purplish-layout/can_be_constrained.rb
@@ -29,10 +29,10 @@ module CanBeConstrained
     result
   end
 
-  def filled_by(aView)
+  def filled_by(aView, insets=NSEdgeInsetsMake(0, 0, 0, 0))
     ['celf', self, 'v', aView].constraints do |celf, v, _|
-      celf.h '|[v]|'
-      celf.v '|[v]|'
+      celf.h '|-l-[v]-r-|', {'l'=>insets.left, 'r'=>insets.right}
+      celf.v '|-t-[v]-b-|', {'t'=>insets.top, 'b'=>insets.bottom}
     end
   end
 end
